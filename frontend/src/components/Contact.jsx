0a803b127f35534d769cf99fe3ce3c89270031ac
AnkitKumarDelta <ankitna355@gmail.com>
2024-03-27 13:21:36 +0530

    last commit

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -12,7 +12,7 @@ const Contact = () => {
     e.preventDefault();
     await axios
       .post(
-        "http://localhost:4000/api/v1/message/send",
+        "https://bright-flannel-nightgown-bass.cyclic.app/api/v1/message/send",
         {
           name,
           email,
